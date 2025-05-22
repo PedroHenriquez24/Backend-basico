36df066f50e43a4d842f63327f665f9a4ed00ee5
Pedro <pedrohenriiquez.2008@gmail.com>
2025-05-22 09:11:47 -0300

    Primera chamba

---
+++ Primera clase backend lean ashee/index.js
@@ -0,0 +1,68 @@
+console.log("hola")
+
+const alumno1 = {
+    nombre: "Pedri",
+    apellido: "Henri",
+    dni: 48434739,
+    anio: 5,
+    curso: "A",
+    nota: 10
+}
+const alumno2 = {
+    nombre: "Nicolas",
+    apellido: "Battias",
+    dni: 48855329,
+    anio: 4,
+    curso: "A",
+    nota: 1
+}
+const alumno3 = {
+    nombre: "Ranzo",
+    apellido: "Ranzovich",
+    dni: 48123456,
+    anio: 3,
+    curso: "B",
+    nota: 0
+}
+const alumno4 = {
+    nombre: "Girotti",
+    apellido: "MiAmigo",
+    dni: 48654321,
+    anio: 4,
+    curso: "B",
+    nota: 10
+}
+const alumno5 = {
+    nombre: "Girotti",
+    apellido: "MiEnemigo",
+    dni: 48635241,
+    anio: 4,
+    curso: "B",
+    nota: 3
+}
+const alumno6 = {
+    nombre: "Leucho",
+    apellido: "Hermoso",
+    dni: 48999999,
+    anio: 5,
+    curso: "A",
+    nota: 11
+}
+const alumno7 = {
+    nombre: "Ivo",
+    apellido: "Pelado",
+    dni: 48000000,
+    anio: 3,
+    curso: "B",
+    nota: 9
+}
+const alumno8 = {
+    nombre: "Martin",
+    apellido: "Bioscaca",
+    dni: 48923558,
+    anio: 4,
+    curso: "A",
+    nota: 5
+}
+
+let arrayAlumnos = [alumno1, alumno2, alumno3, alumno4, alumno5, alumno6, alumno7, alumno8]
